1f1bdc79e2ac3679653975cbd93e3a18a4b21e65
Andrea <andreaarcu@gmail.com>
2024-05-28 15:15:14 +0200

    first commit

--- src/HelloWorld1.jsx
+++ src/HelloWorld1.jsx
@@ -1,6 +1,5 @@
-import Message from "./Message";
 import HelloWorld from "./HelloWorld";
-import AgeCheck from "./Age";
+import AlertClock from "../../AlertClock/AlertClock";
 
 export function HelloWorld1() {
   const age1 = 22;
@@ -11,13 +10,7 @@ export function HelloWorld1() {
       <h2>My awesome Application</h2>
       <hr />
       <HelloWorld />
-      <Message name="Andrea" />
-      {age1 > 18 && <AgeCheck age={age1} />}
-      {age1 && <AgeCheck age={age1} />}
-      {18 < age1 && age1 < 65 && <AgeCheck age={age1} />}
-      {18 < age1 && age1 < 65 && name1 === "john" && (
-        <AgeCheck age={age1} name={name1} />
-      )}
+      <AlertClock />
     </div>
   );
 }
